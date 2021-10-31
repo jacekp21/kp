9bf15a22fc0566a08e44927d73a8d2dff6558d7c
Hegun <hendra.yap@gmail.com>
2021-10-31 16:08:59 +0700

    Add Dropdownbox

--- resources/views/admin/new.blade.php
+++ resources/views/admin/new.blade.php
@@ -19,21 +19,32 @@
   </ol>
 </nav>
             <form>
-            <h1>New Account</h1>
-            <div class="mb-3">
-                <br><label for="username" class="form-label">Username</label></br>
-                <input type="text" class="form-control" id="username" placeholder="Username">
-            </div>
-            <div class="mb-3">
-                <br><label for="password" class="form-label">Password</label></br>
-                <input type="password" class="form-control" id="password" placeholder="Password">
-            </div>
+                <h1>New Account</h1>
                 <div class="mb-3">
-                <br><label for="telp" class="form-label">No. Telp</label></br>
-                <input type="text" class="form-control" id="telp" placeholder="No.Telp">
-            </div>
-            <br><button type="submit" class="btn btn-primary">Save</button></br>
-            <br><button type="reset" class="btn btn-primary">Reset</button></br>
+                    <br><label for="username" class="form-label">Username</label></br>
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="mb-3">
+                    <br><label for="password" class="form-label">Password</label></br>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleDataList" class="form-label">Position</label>
+                    <input class="form-control" list="positionOptions" id="position" placeholder="Type to search...">
+                    <datalist id="positionOptions">
+                        <option value="San Francisco">
+                        <option value="New York">
+                        <option value="Seattle">
+                        <option value="Los Angeles">
+                        <option value="Chicago">
+                    </datalist>
+                </div>
+                <div class="mb-3">
+                    <br><label for="telp" class="form-label">No. Telp</label></br>
+                    <input type="text" class="form-control" id="telp" placeholder="No.Telp">
+                </div>
+                <br><button type="submit" class="btn btn-primary">Save</button></br>
+                <br><button type="reset" class="btn btn-primary">Reset</button></br>
             </form>
                 
         </div>
