8f7b1eefe427f94ead603cb7b113392becbaa6a6
Hegun <azestron58@yahoo.com.sg>
2022-02-20 15:42:37 +0700

    Update Admin and Admin New file

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -11,41 +11,103 @@
     <script src="https://kit.fontawesome.com/cc8db81d9c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="fontawesome-free-6.0.0-beta3-web/css/all.min.css"/>
 
-    <h1>Data Users</h1>
-      <a href="http://localhost:8000/admin/new"><button class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">New </button></a>
+    <style>
+        .nav-link:hover {
+            background-color: grey;
+        }
+        .display-6{
+            font-weight: bold;
+            margin-left: 5px;
+        }
+        .card-body-icon{
+            position: absolute;
+            z-index: 0;
+            top: 25px;
+            right: 4px;
+            opacity: 0.4;
+            font-size: 90px;
+        }
+    </style>
+    <title>Admin Page</title>
+  </head>
+        <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white"><i class="fas fa-user"></i> WELCOME ADMIN | PT BANGUN PRIMA ABADI</a>
+            <a href="http://localhost:8000/login/" class="btn btn-primary mt-3" role="button"><i class="fas fa-sign-out-alt mr-2"></i> Logout</a>
+        </div>
+        </nav>
+        <div class="row no-gutters mt-5">
+            <div class="col-md-2 bg-dark mt-2 pr-3 pt-4">
+            <ul class="nav flex-column ml-3 mb-5">
+            <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="http://localhost:8000/dashboard"><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/admin/admin"><i class="fas fa-user-cog mr-2"></i> Admin</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/po"><i class="fas fa-file-alt mr-2"></i> Purchase Order</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/ap/ap"><i class="fas fa-file-alt mr-2"></i> Account Payable</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/payment"><i class="fas fa-cash-register mr-2"></i> Payment</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/report/report"><i class="fas fa-file-invoice mr-2"></i> Report</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link text-white" href="http://localhost:8000/setting/setting"><i class="fas fa-cog mr-2"></i> Setting</a><hr class="bg-secondary">
+            </li>
+            </ul>
+            </div>
+            <div class="col-md-10 p-5 mt-2">
+                <h1><i class="fas fa-user-cog mr-2"></i> Admin</h1><hr>
+                <a href="http://localhost:8000/admin/new" class="btn btn-primary mt-2 mb-3" role="button"><i class="fas fa-plus-square"></i> Add New</a>
     <div>
-      <table class="table table-striped align-middle">
-        <thead>
-          <th scope="col">No.</th>
-          <th scope="col">Username</th>
-          <th scope="col">Position</th> 
-          <th scope="col">No. Telp</th>
-          <th scope="col">Role</th>
-          <th scope="col">Picture</th>
-          <th scope="col">Action</th>
-        </thead>
-        <tbody>
-            <tr>
-            <td>1</td>
-            <td>Yonna Christiana</td>
-            <td>Administrator</td>
-            <td>082299008899</td>
-            <td>Admin</td>
-            <td>-</td>
-            <td>Update/Void</td>
-            </tr>
-            <tr>
-            <td>2</td>
-            <td>Crystina</td>
-            <td>Purchaser</td>
-            <td>081299003333</td>
-            <td>Staff</td>
-            <td>-</td>
-            <td>Update/Void</td>
-            </tr>
-        </tbody>
-      </table>   
-    </div>
-    <a href="http://localhost:8000/">Home</a>
+                    <table class="table table-striped align-middle table-bordered">
+                        <thead>
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Position</th>
+                            <th scope="col">No. Telp</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Status</th> 
+                            <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>1</td>
+                            <td>Yonna Christiana</td>
+                            <td>Administrator</td>
+                            <td>082299008899</td>
+                            <td>Admin</td>
+                            <td>-</td> <!-- <img src="langit.png"> -->
+                            <td>
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-edit"></i> Update</button>
+                            <button type="submit" class="btn btn-danger"><i class="fas fa-ban"></i> Void</button>
+                            </td> 
+                            </tr>
+                            <tr>
+                            <td>2</td>
+                            <td>Crystina</td>
+                            <td>Purchaser</td>
+                            <td>081299003333</td>
+                            <td>Staff</td>
+                            <td></td> <!-- tombol switch -->
+                            <td>
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-edit"></i> Update</button>
+                            <button type="submit" class="btn btn-danger"><i class="fas fa-ban"></i> Void</button>
+                            </td>
+                            </tr>
+                        </tbody>
+                        </table>   
+                </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/admin/new.blade.php
+++ resources/views/admin/new.blade.php
@@ -12,79 +12,65 @@
     <link rel="stylesheet" href="fontawesome-free-6.0.0-beta3-web/css/all.min.css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>New Account</title>
-
-    <style>
-            body
-                {
-                background-color:#f5f5f5;
-                }
-            .imagePreview {
-                    width: 235px;
-                    height: 200px;
-                    background-position: center center;
-                    background:url(http://cliquecities.com/assets/no-image-e3699ae23f866f6cbdf8ba2443ee5c4e.jpg);
-                    background-color:#fff;
-                    background-size: cover;
-                    background-repeat:no-repeat;
-                    background-size: 250px 200px;
-                    display: inline-block;
-                    box-shadow:0px -3px 6px 2px rgba(0,0,0,0.2);
-                    margin-bottom: 5px;
-                }
-            .btn-primary
-                {
-                display:block;
-                border-radius:0px;
-                box-shadow:0px 4px 6px 2px rgba(0,0,0,0.2);
-                margin-top:-5px;
-                }
-            .imgUp
-                {
-                    margin-bottom:15px;
-                }
-            .del
-                {
-                position:absolute;
-                top:0px;
-                right:15px;
-                width:30px;
-                height:30px;
-                text-align:center;
-                line-height:30px;
-                background-color:rgba(255,255,255,0.6);
-                cursor:pointer;
-                }
-            .imgAdd
-                {
-                width:30px;
-                height:30px;
-                border-radius:50%;
-                background-color:#4b8def;
-                color:#fff;
-                box-shadow:0px 0px 2px 1px rgba(0,0,0,0.2);
-                text-align:center;
-                line-height:30px;
-                margin-top:0px;
-                cursor:pointer;
-                font-size:15px;
-                }
-        </style>
-</head>
-<body>
-    <div class="container mt-5">
-            <div class="row">
-                <div class="col-md-6">
-    <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="http://localhost:8000/dashboard">Home</a></li>
-        <li class="breadcrumb-item"><a href="http://localhost:8000/admin/admin">Admin</a></li>
-        <li class="breadcrumb-item active" aria-current="page">New</a></li>
-    </ol>
-    </nav>
-            <form>
-                <h1>New Account</h1>
-                <div class="mb-3">
+            <style>
+        .nav-link:hover {
+            background-color: grey;
+        }
+        .display-6{
+            font-weight: bold;
+            margin-left: 5px;
+        }
+        .card-body-icon{
+            position: absolute;
+            z-index: 0;
+            top: 25px;
+            right: 4px;
+            opacity: 0.4;
+            font-size: 90px;
+        }
+    </style>
+    <title>New Admin Page</title>
+  </head>
+        <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white"><i class="fas fa-user"></i> WELCOME ADMIN | PT BANGUN PRIMA ABADI</a>
+            <a href="http://localhost:8000/login/" class="btn btn-primary mt-3" role="button"><i class="fas fa-sign-out-alt mr-2"></i> Logout</a>
+        </div>
+        </nav>
+        <div class="row no-gutters mt-5">
+            <div class="col-md-2 bg-dark mt-2 pr-3 pt-4">
+            <ul class="nav flex-column ml-3 mb-5">
+            <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="http://localhost:8000/dashboard"><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/admin/admin"><i class="fas fa-user-cog mr-2"></i> Admin</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/po"><i class="fas fa-file-alt mr-2"></i> Purchase Order</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/ap/ap"><i class="fas fa-file-alt mr-2"></i> Account Payable</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/payment"><i class="fas fa-cash-register mr-2"></i> Payment</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="http://localhost:8000/report/report"><i class="fas fa-file-invoice mr-2"></i> Report</a><hr class="bg-secondary">
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link text-white" href="http://localhost:8000/setting/setting"><i class="fas fa-cog mr-2"></i> Setting</a><hr class="bg-secondary">
+            </li>
+            </ul>
+            </div>
+            <div class="col-md-10 p-5 mt-2">
+                <h1><i class="fas fa-user-cog mr-2"></i> New Account</h1><hr>
+                <div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="reset" class="btn btn-primary">Reset</button>
+                </div>
+                <div class="pt-3 mb-1">
                     <label for="username" class="form-label">Username</label>
                     <input type="text" class="form-control" id="username" placeholder="Username">
                 </div>
@@ -131,22 +117,6 @@
                         frame.src = "";
                     }
                 </script>
-                <div>
-                    <button type="submit" class="btn btn-primary">Save</button>
-                    <button type="reset" class="btn btn-primary">Reset</button>
-                </div>
-                <br>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6 imgUp">
-                            <div class="imagePreview"></div>
-                            <label class="btn btn-primary">Upload<input type="file" class="uploadFile img" value="Upload Photo" style="width: 0px;height: 0px;overflow: hidden;"></label>
-                        </div><!-- col-2 -->
-                        <i class="fa fa-plus imgAdd"></i>
-                    </div><!-- row -->
-                </div><!-- container -->
-            </form>
-            <a href="http://localhost:8000/">Home</a>
         </div>
     </div>
 </body>
