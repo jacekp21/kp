608ab8659909fa6ff1a4649a206087484e62a12a
Hendra Gunawan <hendra.yap@gmail.com>
2022-03-07 16:21:17 +0700

    created layout file
    simplify layout

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -40,38 +40,8 @@
             </div>
         </nav>
         <div class="row no-gutters mt-5">
-            <div class="col-md-2 bg-dark mt-2 pr-3 pt-4">
-                <ul class="nav flex-column ml-3 mb-5">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" aria-current="page" href="http://localhost:8000/dashboard"><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="http://localhost:8000/admin/admin"><i class="fas fa-user-cog mr-2"></i> Admin</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="http://localhost:8000/po"><i class="fas fa-file-alt mr-2"></i> Purchase Order</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="http://localhost:8000/ap/ap"><i class="fas fa-file-alt mr-2"></i> Account Payable</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="http://localhost:8000/payment"><i class="fas fa-cash-register mr-2"></i> Payment</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="http://localhost:8000/report/report"><i class="fas fa-file-invoice mr-2"></i> Report</a><hr class="bg-secondary">
-                    </li>
-                    <li class="nav-item has-submenu">
-                        <a class="nav-link text-white" href="http://localhost:8000/setting/setting"> 
-                            <i class="fas fa-cog mr-2"></i> Setting
-                        </a>
-                        <ul class="submenu collapse">
-                            <li><a class="nav-link text-white" href="#">Vendor </a></li>
-                            <li><a class="nav-link text-white" href="#">Warehouse </a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-
+            @section('sidebar')
+                @@parent
             <div class="col-md-10 p-5 mt-2">
                 <h1><i class="fas fa-user-cog mr-2"></i> Admin</h1><hr>
                 <a href="http://localhost:8000/admin/new" class="btn btn-primary mt-2 mb-3" role="button"><i class="fas fa-plus-square"></i> Add New</a>
