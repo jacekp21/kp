084bc438bd4e8327a0e54269035befd542088378
Hegun <azestron58@yahoo.com.sg>
2022-03-07 16:15:08 +0700

    create layout sidebar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="col-md-2 bg-dark mt-2 pr-3 pt-4">
+    <ul class="nav flex-column ml-3 mb-5">
+        <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href={{ url("dashboard") }}><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href={{ url("admin/admin") }}><i class="fas fa-user-cog mr-2"></i> Admin</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href={{ url("po") }}><i class="fas fa-file-alt mr-2"></i> Purchase Order</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href={{ url("ap/ap") }}><i class="fas fa-file-alt mr-2"></i> Account Payable</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href={{ url("payment") }}><i class="fas fa-cash-register mr-2"></i> Payment</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href={{ url("report/report") }}><i class="fas fa-file-invoice mr-2"></i> Report</a><hr class="bg-secondary">
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link text-white" href={{ url("setting/setting") }}><i class="fas fa-cog mr-2"></i> Setting</a><hr class="bg-secondary">
+        </li>
+    </ul>
+</div>
