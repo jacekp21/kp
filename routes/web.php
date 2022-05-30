a2010fbb90ac77f2e3ff2f1c301c7d196e1d6ab9
jacelyn <jacelynhuang8@domain.com>
2022-05-30 15:40:23 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PoController;
+use App\Http\Controllers\ApController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\WarehouseController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 
@@ -47,20 +49,34 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/setting/vendor/edit/{id}', [VendorController::class, 'edit']);
     Route::get('/setting/vendor/delete/{id}', [VendorController::class, 'delete']);
 
+    // Warehouse
+    Route::get('/setting/wh', [WarehouseController::class, 'index']);
+    Route::get('/setting/wh/new', [WarehouseController::class, 'create']);
+    Route::post('/setting/wh/store', [WarehouseController::class, 'store']); // Store Warehouse Information
+    Route::get('/setting/wh/edit/{id}', [WarehouseController::class, 'edit']);
+    Route::get('/setting/wh/delete/{id}', [WarehouseController::class, 'delete']);
+
+    // Purchase Order
+    Route::get('/po', [PoController::class, 'index']);    
+    Route::get('/po/new', [PoController::class, 'create']);
+    Route::post('/po/store', [PoController::class, 'store']); // Store Purchase Order Information
+    Route::get('/po/edit/{id}', [PoController::class, 'edit']);
+    Route::get('/po/void/{id}', [PoController::class, 'void']);
+    Route::get('/po/show/{id}', [PoController::class, 'show']);
+
+    // Account Payable
+    Route::get('/ap', [PoController::class, 'index']);    
+    Route::get('/ap/new', [PoController::class, 'create']);
+    Route::post('/ap/store', [PoController::class, 'store']); // Store Account Payable Information
+    Route::get('/ap/edit/{id}', [PoController::class, 'edit']);
+    Route::get('/ap/void/{id}', [PoController::class, 'void']);
+    Route::get('/ap/show/{id}', [PoController::class, 'show']);
 });
 
 // Route::resource('setting/vendor/new', [VendorController::class, 'create']);
 // Route::get('setting/vendor/new', function () {
 //     return view('setting/vendor/new');
 // });
-
-
-Route::get('/po', [PoController::class, 'index']);
-Route::get('/po/show/{id}', [PoController::class, 'show']);
-
-Route::get('/po/new', function () {
-    return view('po/new');
-});
 
 Route::get('/ap', function () {
     return view('ap/index');
@@ -86,13 +102,13 @@ Route::get('/setting', function () {
     return view('setting/index');
 });
 
-Route::get('setting/wh', function () {
-    return view('setting/wh/index');
-});
+// Route::get('setting/wh', function () {
+//     return view('setting/wh/index');
+// });
 
-Route::get('setting/wh/new', function () {
-    return view('setting/wh/new');
-});
+// Route::get('setting/wh/new', function () {
+//     return view('setting/wh/new');
+// });
 
 
 //Route::get('/', [LoginController::class, 'login'])->name('login');
