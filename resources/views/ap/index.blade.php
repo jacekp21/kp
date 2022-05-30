2c1142356213618580833740acd6febdf42142ec
jacelyn <jacelynhuang8@domain.com>
2022-05-30 11:25:26 +0700

    Perubahan AP ( style , body ) error layout.header

--- resources/views/ap/index.blade.php
+++ resources/views/ap/index.blade.php
@@ -9,7 +9,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!-- Font Awesome Link -->
         <script src="https://kit.fontawesome.com/cc8db81d9c.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="fontawesome-free-6.0.0-beta3-web/css/all.min.css"/>
 
         <style>
             .nav-link:hover {
@@ -31,12 +30,7 @@
         <title>Account Payable Page</title>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand text-white"><i class="fas fa-user"></i> WELCOME ADMIN | PT BANGUN PRIMA ABADI</a>
-            <a href="http://localhost:8000/login/" class="btn btn-primary mt-3" role="button"><i class="fas fa-sign-out-alt mr-2"></i> Logout</a>
-        </div>
-        </nav>
+        @include('layouts.header')
         <div class="row no-gutters mt-5">
             @include('layouts.sidebar')
             <div class="col-md-10 p-5 mt-2">
