70fcff7a82346d2c1e6fb2e713c8362640ad49e6
jacelyn <jacelynhuang8@domain.com>
2022-08-11 12:25:56 +0700

    Update APR PDF

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,33 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Report;
+use App\Models\Apr;
 use App\Models\Vendor;
 use App\Models\Warehouse;
 use Barryvdh\DomPDF\Facade\Pdf;
 
-
 class ReportController extends Controller
 {
-  public function index()
-  {
-    $aprs = Report::all();
-    return view('/report/apr',['aprs'=>$aprs]);
-  }
+    public function index()
+    {
+        $apr = Apr::get();
+        return view('report.apr')->with('apr', $apr);
+    }
+    
+    public function print()
+    {
+    $data['apr'] = Apr::with('vendor');
 
-  public function indexpy()
-  {
-    $pyrs = Report::all();
-    return view('/report/pyr',['pyrs'=>$pyrs]);
-  }
+    $pdf = Pdf::loadView('report.apr_pdf', $data);
 
-
-  public function cetak_pdf()
-  {
-      $data['Report'] = Report::with('Report')->with('vendor')->with('warehouse')->findOrFail($id);
-      $data['no'] = 0;
-      $pdf = Pdf::loadView('report.pdf', $data);
-  
-      return $pdf->stream('report-'.$data['report'].'pdf');
-  }
+    return $pdf->stream('apr.pdf');
+    }
 }
